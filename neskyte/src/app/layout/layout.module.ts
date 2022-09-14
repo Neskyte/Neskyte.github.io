0ff5edb1bb41a17070f4a3cc546ca2c0a302c3d3
Nathanael MAYA UNGLAT <nathanael@ringring.be>
2022-09-14 21:35:26 +0200

    prototype menu

---
+++ neskyte/src/app/layout/layout.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+
+@NgModule({
+  declarations: [MenuBarComponent],
+  imports: [CommonModule],
+  exports: [MenuBarComponent],
+})
+export class LayoutModule {}
